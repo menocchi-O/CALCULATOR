b7417ff6d7d49e489001f3d400cce2db961e47f1
Menocchi-O <rivolta.fabio@outlook.it>
2023-03-11 17:56:03 +0100

    Added Application to call the Calculator

--- Application.java
+++ Application.java
@@ -1,1 +1,9 @@
-
+public class Application{
+	public static void main (String[] args){
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.addition(1,2));
+		//should be 3
+		System.out.println(calculator.addition(1.5,2.2));
+		//should be 4.7
+	}
+}
